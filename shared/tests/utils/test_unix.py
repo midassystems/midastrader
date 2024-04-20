8ba8ced2cc65d6857de9740e11bf35bebfe81040
anthony baxter <anthony_baxter@student.uml.edu>
2024-04-20 15:03:57 -0400

    UPD: Updated unix.py to convert to & from ISO

--- shared/tests/utils/test_unix.py
+++ shared/tests/utils/test_unix.py
@@ -3,24 +3,46 @@ import pandas as pd
 from unittest.mock import Mock, patch
 from contextlib import ExitStack
 from decimal import Decimal
-from shared.utils.unix import convert_to_unix
+from shared.utils.unix import iso_to_unix, unix_to_iso
 
 class TestConvertToUnix(unittest.TestCase):
-    def test_date_format_1(self):
-        timestamp_1="2024-01-10T12:00:00"
+    def test_iso_to_unix_tz_aware(self):
+        iso_timestamp = "2024-02-07T12:00:00+00:00" 
+        
         # test
-        unix_1=convert_to_unix(timestamp_1)
+        unix_timestamp=iso_to_unix(iso_timestamp)
 
         # valdiate
-        self.assertEqual(unix_1,1704888000)
+        self.assertEqual(unix_timestamp,1707307200)
 
-    def test_date_format_2(self):
-        timestamp_2="2023-01-10 12:00:00"
+
+    def test_iso_to_unix_tz_naive(self):
+        iso_timestamp = "2023-02-07T12:00:00" 
+        
         # test
-        unix_2=convert_to_unix(timestamp_2)
+        unix_timestamp=iso_to_unix(iso_timestamp)
 
         # valdiate
-        self.assertEqual(unix_2,1673352000)
+        self.assertEqual(unix_timestamp,1675789200)
+
+    def test_unix_to_ios_UTC(self):
+        unix_timestamp =  1679789200
+        
+        # test
+        iso_timestamp=unix_to_iso(unix_timestamp)
+
+        # valdiate
+        self.assertEqual(iso_timestamp,"2023-03-26T00:06:40+00:00")
+
+    def test_unix_to_ios_EST(self):
+        unix_timestamp =  1679789200
+        
+        # test
+        iso_timestamp=unix_to_iso(unix_timestamp, "US/Eastern")
+
+        # valdiate
+        self.assertEqual(iso_timestamp,"2023-03-25T20:06:40-04:00")
+
 
 if __name__ =="__main__":
     unittest.main()
