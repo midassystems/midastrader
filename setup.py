15bc2734a57a40e1af22cd80296753c95439e19f
anthony baxter <anthony_baxter@student.uml.edu>
2024-06-20 20:24:24 -0400

    MOD: Modified setup description format.

--- setup.py
+++ setup.py
@@ -9,7 +9,19 @@ setup(
     version='0.1.0',  # Version number
     author='Anthony Baxter', 
     author_email='anthony_baxter819@gmail.com',  
-    description="A comprehensive suite designed for advanced financial analysis and trading, this package combines a robust management tool for Django Rest Framework (DRF) API backends with powerful financial data retrieval capabilities, ensuring efficient integration and manipulation of financial datasets. It includes an event-driven backtesting and live trading library tailored for compatibility with the Interactive Brokers API, supporting the development, testing, and deployment of complex trading strategies with real-time data and execution. Additionally, the suite features an efficient, vectorized backtesting library optimized for quantitative analysis, providing advanced tools for high-speed, precise evaluation of trading strategies. This all-in-one toolkit is ideal for financial analysts, quantitative researchers, and traders seeking a reliable, integrated platform for comprehensive financial management and strategy development.", # Short description
+    description="""A comprehensive suite designed for advanced financial analysis and trading, 
+                this package combines a robust management tool for Django Rest Framework (DRF) 
+                API backends with powerful financial data retrieval capabilities, ensuring 
+                efficient integration and manipulation of financial datasets. It includes 
+                an event-driven backtesting and live trading library tailored for compatibility 
+                with the Interactive Brokers API, supporting the development, testing, and 
+                deployment of complex trading strategies with real-time data and execution. 
+                Additionally, the suite features an efficient, vectorized backtesting 
+                library optimized for quantitative analysis, providing advanced tools for 
+                high-speed, precise evaluation of trading strategies. This all-in-one 
+                toolkit is ideal for financial analysts, quantitative researchers, and 
+                traders seeking a reliable, integrated platform for comprehensive 
+                financial management and strategy development.""", # Short description
     long_description=open('README.md').read(),  # Detailed description from README.md
     long_description_content_type='text/markdown',  # Specifies the long desc is in Markdown
     url='https://github.com/anthonyb8/MidasPython.git',  # Project home page or repository URL
