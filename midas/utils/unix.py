77815e1c05fb97421bd5bb3cba1bb3b3a14051ab
anthony baxter <anthony_baxter@student.uml.edu>
2024-10-01 08:22:22 -0400

    UPD: Updated utils and tests.

--- midas/utils/unix.py
+++ midas/utils/unix.py
@@ -2,6 +2,7 @@ import pytz
 import pandas as pd
 from datetime import datetime, timezone
 
+
 def iso_to_unix(timestamp_str: str):
     """
     Converts an ISO 8601 formatted date string to a UNIX timestamp in nanoseconds.
@@ -21,13 +22,16 @@ def iso_to_unix(timestamp_str: str):
         dt = datetime.fromisoformat(timestamp_str)
     except ValueError:
         # If no timezone is specified, assume UTC
-        dt = datetime.fromisoformat(timestamp_str + 'Z').replace(tzinfo=timezone.utc)
+        dt = datetime.fromisoformat(timestamp_str + "Z").replace(
+            tzinfo=timezone.utc
+        )
 
     # Convert to Unix timestamp (seconds since the epoch, with nanoseconds)
     unix_timestamp = int(dt.timestamp() * 1e9)
     return unix_timestamp
 
-def unix_to_iso(unix_timestamp: int, tz_info='UTC'):
+
+def unix_to_iso(unix_timestamp: int, tz_info="UTC"):
     """
     Converts a UNIX timestamp in nanoseconds to an ISO 8601 formatted string, with an optional timezone.
 
@@ -43,19 +47,58 @@ def unix_to_iso(unix_timestamp: int, tz_info='UTC'):
     - str: An ISO 8601 formatted datetime string in the specified timezone.
     """
     # Convert Unix timestamp to datetime object in UTC
-    dt_utc = datetime.fromtimestamp(unix_timestamp  / 1e9, tz=timezone.utc)
+    dt_utc = datetime.fromtimestamp(unix_timestamp / 1e9, tz=timezone.utc)
 
     # Check if a specific timezone is requested
-    if tz_info != 'UTC':
+    if tz_info != "UTC":
         tz = pytz.timezone(tz_info)
         dt_tz = dt_utc.astimezone(tz)
         return dt_tz.isoformat()
     else:
         return dt_utc.isoformat()
 
-def resample_daily(df:pd.DataFrame, tz_info='UTC'):
+
+def unix_to_date(unix_timestamp: int, tz_info="UTC") -> datetime.date:
     """
-    Converts a DataFrame with UNIX timestamp index to daily resolution.
+    Converts a UNIX timestamp in nanoseconds to an ISO 8601 formatted date string, with an optional timezone.
+
+    This function takes a UNIX timestamp in nanoseconds and converts it into a datetime object. The datetime
+    is initially set in UTC. If a different timezone is specified, the datetime is converted to that timezone
+    before formatting it into an ISO 8601 date string (YYYY-MM-DD).
+
+    Parameters:
+    - unix_timestamp (int): The UNIX timestamp in nanoseconds since the epoch.
+    - tz_info (str): A string representing the timezone for the resulting ISO string. Defaults to 'UTC'.
+
+    Returns:
+    - str: An ISO 8601 formatted date string in the specified timezone (YYYY-MM-DD).
+    """
+    # Convert Unix timestamp to datetime object in UTC
+    dt_utc = datetime.fromtimestamp(unix_timestamp / 1e9, tz=timezone.utc)
+
+    # Check if a specific timezone is requested
+    if tz_info != "UTC":
+        tz = pytz.timezone(tz_info)
+        dt_tz = dt_utc.astimezone(tz)
+        return dt_tz.date()
+    else:
+        return dt_utc.date()
+
+
+def _convert_timestamp(
+    df: pd.DataFrame,
+    column: str = "timestamp",
+    tz_info: str = "UTC",
+) -> None:
+    """Converts a dataframe column to iso from unix."""
+    df[column] = pd.to_datetime(
+        df[column].map(lambda x: unix_to_iso(x, tz_info))
+    )
+
+
+def resample_timestamp(df: pd.DataFrame, interval: str = "D", tz_info="UTC"):
+    """
+    Converts a DataFrame with UNIX timestamp index to default daily resolution.
 
     Parameters:
     - df (pd.DataFrame): DataFrame with UNIX timestamp index.
@@ -67,24 +110,28 @@ def resample_daily(df:pd.DataFrame, tz_info='UTC'):
     """
     utc = True
 
-    if tz_info != 'UTC':
-        utc=False
-    
+    if tz_info != "UTC":
+        utc = False
+
     # Convert index to readable datetime
-    df.index = pd.to_datetime(df.index.map(lambda x: unix_to_iso(x, tz_info)), utc=utc)
+    df.index = pd.to_datetime(
+        df.index.map(lambda x: unix_to_iso(x, tz_info)), utc=utc
+    )
 
     # Store original UNIX timestamps before resampling
-    original_timestamps = df.index.to_series().resample('D').last()
+    original_timestamps = df.index.to_series().resample(interval).last()
     original_timestamps.dropna(inplace=True)
 
     # Resample to daily frequency, using the last value of each day
-    daily_df = df.resample('D').last()
+    daily_df = df.resample(interval).last()
 
     # Optionally, fill NaN values if necessary, depending on your specific needs
     daily_df.dropna(inplace=True)
 
     # Restore original UNIX timestamps
-    daily_df.index = original_timestamps.map(lambda x: iso_to_unix(x.isoformat()))
-    daily_df.index.name = 'timestamp'
+    daily_df.index = original_timestamps.map(
+        lambda x: iso_to_unix(x.isoformat())
+    )
+    daily_df.index.name = "timestamp"
 
     return daily_df
---
+++ tests/unit/util/test_unix.py
@@ -0,0 +1,106 @@
+import unittest
+import pandas as pd
+from midas.utils.unix import (
+    unix_to_iso,
+    iso_to_unix,
+    unix_to_date,
+    resample_timestamp,
+)
+import datetime
+
+
+class TestUtils(unittest.TestCase):
+    def test_iso_to_unix_tz_aware(self):
+        iso_timestamp = "2024-02-07T12:00:00+00:00"
+
+        # Test
+        unix_timestamp = iso_to_unix(iso_timestamp)
+
+        # Valdiate
+        self.assertEqual(unix_timestamp, 1707307200000000000)
+
+    def test_iso_to_unix_naive(self):
+        date_str = "2021-11-01T01:01:01Z"
+
+        # Test
+        unix_nanos = iso_to_unix(date_str)
+
+        # Validate
+        self.assertEqual(1635728461000000000, unix_nanos)
+
+    def test_unix_to_iso_utc(self):
+        unix = 1635728461000000000
+
+        # Test
+        iso = unix_to_iso(unix)
+
+        # Validate
+        self.assertEqual("2021-11-01T01:01:01+00:00", iso)
+
+    def test_unix_to_ios_EST(self):
+        unix_timestamp = 1679789200000000000
+
+        # Test
+        iso_timestamp = unix_to_iso(unix_timestamp, "US/Eastern")
+
+        # Valdiate
+        self.assertEqual(iso_timestamp, "2023-03-25T20:06:40-04:00")
+
+    def test_unix_to_date(self):
+        unix = 1635728461000000000
+
+        # Test
+        date = unix_to_date(unix)
+
+        # Validate
+        self.assertEqual(datetime.date(2021, 11, 1), date)
+
+    def test_resample_daily(self):
+        df = pd.DataFrame(
+            {
+                "timestamp": [
+                    1704098576204104704,
+                    1704119427481684736,
+                    1704159224811226368,
+                    1704203604070369024,
+                    1704254892183270144,
+                    1704312309960591616,
+                    1704354268672059648,
+                    1704399080573728256,
+                    1704450706695355136,
+                    1704497748117854720,
+                    1704540253016760064,
+                    1704573685899493888,
+                    1704629115124939264,
+                    1704673128279459840,
+                ],
+                "data": [1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5],
+            }
+        ).set_index("timestamp")
+
+        # Test
+        daily_df = resample_timestamp(df)
+
+        # Excpected
+        expected_df = pd.DataFrame(
+            {
+                "timestamp": [
+                    1704119427481684736,
+                    1704203604070369024,
+                    1704312309960591616,
+                    1704399080573728256,
+                    1704497748117854720,
+                    1704573685899493888,
+                    1704629115124939264,
+                    1704673128279459840,
+                ],
+                "data": [2, 4, 6, 8, 1, 3, 4, 5],
+            }
+        ).set_index("timestamp")
+
+        # Validate
+        pd.testing.assert_frame_equal(daily_df, expected_df)
+
+
+if __name__ == "__main__":
+    unittest.main()
