dda70c9a7fb7958b9c13b4b14fa5e85ef1280d61
anthony baxter <anthony_baxter@student.uml.edu>
2025-01-06 08:01:03 -0500

    ADD: Added README, LICENSE and examples.

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
 
 # Read requirements.txt and use its contents as the install_requires list
-with open('requirements.txt') as f:
+with open("requirements.txt") as f:
     requirements = f.read().splitlines()
 
 setup(
-    name='midas',  # Your package name
-    version='0.1.0',  # Version number
-    author='Anthony Baxter', 
-    author_email='anthony_baxter819@gmail.com',  
+    name="midas",  # Your package name
+    version="0.1.0",  # Version number
+    author="Anthony Baxter",
+    author_email="anthony_baxter819@gmail.com",
     description="""A comprehensive suite designed for advanced financial analysis and trading, 
                 this package combines a robust management tool for Django Rest Framework (DRF) 
                 API backends with powerful financial data retrieval capabilities, ensuring 
@@ -21,16 +21,23 @@ setup(
                 high-speed, precise evaluation of trading strategies. This all-in-one 
                 toolkit is ideal for financial analysts, quantitative researchers, and 
                 traders seeking a reliable, integrated platform for comprehensive 
-                financial management and strategy development.""", # Short description
-    long_description=open('README.md').read(),  # Detailed description from README.md
-    long_description_content_type='text/markdown',  # Specifies the long desc is in Markdown
-    url='https://github.com/anthonyb8/MidasPython.git',  # Project home page or repository URL
+                financial management and strategy development.""",  # Short description
+    long_description=open(
+        "README.md"
+    ).read(),  # Detailed description from README.md
+    long_description_content_type="text/markdown",  # Specifies the long desc is in Markdown
+    url="https://github.com/anthonyb8/MidasPython.git",  # Project home page or repository URL
     packages=find_packages(),  # Automatically discover all packages and subpackages
     install_requires=requirements,
+    entry_points={
+        "console_scripts": [
+            "midas=midas.cli.main:main",  # Link `midas` command to the `main` function
+        ],
+    },
     classifiers=[
-        'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',  # Example classifier, adjust as needed
-        'Operating System :: OS Independent',
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",  # Example classifier, adjust as needed
+        "Operating System :: OS Independent",
     ],
-    python_requires='>=3.10',  # Minimum Python version requirement
+    python_requires=">=3.10",  # Minimum Python version requirement
 )
