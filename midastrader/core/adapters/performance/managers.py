1bbcd0867704748f4c58f68af15686c7cab8c6c9
anthonyb8 <anthonybaxter819@gmail.com>
2025-03-01 15:27:38 -0500

    Updated equity_value tracking statement to only disregard and equity update if same as the last not prior updates.

--- midastrader/core/adapters/performance/managers.py
+++ midastrader/core/adapters/performance/managers.py
@@ -464,7 +464,7 @@ class EquityManager:
         Args:
             equity_details (EquityDetails): The equity details to be logged.
         """
-        if equity_details not in self.equity_value:
+        if not self.equity_value or equity_details != self.equity_value[-1]:
             self.equity_value.append(equity_details)
             self.logger.debug(
                 f"\nEQUITY UPDATED: \n  {self.equity_value[-1]}\n"
@@ -616,14 +616,15 @@ class EquityManager:
         raw_equity_curve = raw_equity_df["equity_value"].to_numpy()
         daily_returns = self.daily_stats["period_return"].to_numpy()
         period_returns = self.period_stats["period_return"].to_numpy()
-        annualized_return = round(((1 + daily_returns.mean()) ** 252) - 1, 4)
 
         return {
             "net_profit": float(Metrics.net_profit(raw_equity_curve)),
             "beginning_equity": float(raw_equity_curve[0]),
             "ending_equity": float(raw_equity_curve[-1]),
             "total_return": float(Metrics.total_return(raw_equity_curve)),
-            "annualized_return": float(annualized_return),
+            "annualized_return": float(
+                Metrics.annualize_returns(daily_returns)
+            ),
             "daily_standard_deviation_percentage": float(
                 Metrics.standard_deviation(daily_returns)
             ),
